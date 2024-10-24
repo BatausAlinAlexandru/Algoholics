6eb849f8ff5a7b1e9dc5fc63597fa77516e83042
BatausAlinAlexandru <bataus.alin.alexandru@gmail.com>
2024-10-24 12:40:11 +0300

    fix v2

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27c3200d6eaafcf13f2ce00e08eb3f6798c2b127")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e908d3bf25b5e30f4319caff76a0a9db4a2db8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
