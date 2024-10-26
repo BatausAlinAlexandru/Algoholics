2b046cd1b064ae02381f5d96c9d18a5f1a5ec1e1
Butunoi05 <147374590+Butunoi05@users.noreply.github.com>
2024-10-26 21:18:05 +0300

    Product-DP

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e96eedcf09fded276a82ef87d58b69425d1ed8f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab9c30eda2cda5020abb8b9f86cbad9e9b9abfd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Domain/Aggregates/ProductAggregate/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Aggregates.ProductAggregate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    namespace Domain.Aggregates.ProductAggregate
+    {
+        public class Product
+        {
+            private readonly List<DetaliiProduct> products = new List<DetaliiProduct>();
+
+            // Method to add a new Product
+            public void AddProduct(DetaliiProduct product)
+            {
+                if (product == null)
+                    throw new ArgumentNullException(nameof(product));
+
+                products.Add(product);
+            }
+
+          
+            public bool UpdateProduct(DetaliiProduct product, string name, string description, decimal price, int stockQuantity, string category)
+            {
+                if (product == null || !products.Contains(product))
+                    return false;
+
+                product.Update(name, description, price, stockQuantity, category);
+                return true;
+            }
+            public bool DeleteProduct(DetaliiProduct product)
+            {
+                return products.Remove(product);
+            }
+
+            public DetaliiProduct GetProductByName(string name)
+            {
+                return products.FirstOrDefault(p => p.GetName().Equals(name, StringComparison.OrdinalIgnoreCase));
+            }
+        }
+    }
+}
