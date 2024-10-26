ab9c30eda2cda5020abb8b9f86cbad9e9b9abfd6
Butunoi05 <147374590+Butunoi05@users.noreply.github.com>
2024-10-26 19:15:52 +0300

    a

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a153fed9c1101b44d3893dc03f6e70209e37181b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e96eedcf09fded276a82ef87d58b69425d1ed8f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
