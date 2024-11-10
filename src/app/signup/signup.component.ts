058c49ba9f802168def56f6985dd6b3e8d0dfcad
Minimatrix26 <armand.brudea@yahoo.com>
2024-11-10 18:17:53 +0200

    Prima incercare de login/signup page

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+  email = '';
+  username = '';
+  password = '';
+
+  constructor( private authService: AuthService, private router: Router){}
+
+  onSignup() {
+    if (this.authService.signup(this.email, this.username, this.password)) {
+      alert('Sign up successful');
+      this.router.navigate(['/']);
+    } else {
+      alert('Email already in use');
+    }
+  }
+
+}
