e61cd2708e8dd420ea9cbd8f3c63e692498bc799
Butunoi05 <147374590+Butunoi05@users.noreply.github.com>
2024-11-14 00:52:53 +0200

    ProductPictures

---
+++ Domain/Aggregates/ProductAggregate/Entities/ProductPictures.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Aggregates.ProductAggregate.Entities
+{
+  
+    public class ProductPictures
+    {
+        public Guid Id { get; set; }
+        public string Url { get; set; }
+
+        public ProductPictures(string url)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                throw new ArgumentException("Image URL cannot be empty.");
+            }
+
+            Id = Guid.NewGuid();
+            Url = url;
+        }
+
+        public void UpdateUrl(string newUrl)
+        {
+            if (string.IsNullOrWhiteSpace(newUrl))
+            {
+                throw new ArgumentException("New URL cannot be empty.");
+            }
+            Url = newUrl;
+        }
+    }
+}
+
