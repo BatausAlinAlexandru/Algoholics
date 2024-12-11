57f7eff8d2ed878fada2c53f0e1436ffdcc00502
Butunoi05 <147374590+Butunoi05@users.noreply.github.com>
2024-12-11 20:17:00 +0200

    Controller produs

---
+++ Application/Services/Interfaces/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task AddProductAsync(string name, float price, string description, int stoc, int disc, string pathFoto);
+        public Task DeleteProductAsync(Guid IdProd);
+        Task ModiftProductAsync(Guid productId, string name, float price, string description, int stoc, int disc, string pathFoto);
+    }
+}
