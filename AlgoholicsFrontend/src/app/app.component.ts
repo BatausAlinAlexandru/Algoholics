317b806eb7bcc668d557ee1117990d55fc89b50a
Chira Teodora Mihaela <dora.chira10@gmail.com>
2024-12-13 00:49:48 +0200

    login/signup fara header si footer

--- AlgoholicsFrontend/src/app/app.component.ts
+++ AlgoholicsFrontend/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
   title = 'Project1';
+
+  constructor(private router: Router) { }
+
+  // Funcție care verifică dacă ruta curentă este login sau signup
+  showNavbar(): boolean {
+    return !['/login', '/signup'].includes(this.router.url); // Ascunde header-ul pe login și signup
+  }
+
+  // Funcție care verifică dacă ruta curentă este login sau signup
+  showFooter(): boolean {
+    return !['/login', '/signup'].includes(this.router.url); // Ascunde footer-ul pe login și signup
+  }
+
 }
