e36d88b20aad4ed327bf213e0814ab0c53440973
Minimatrix26 <armand.brudea@yahoo.com>
2025-01-11 13:59:17 +0200

    wishlist backend first try

---
+++ Domain/Aggregates/WishlistAggregate/Entities/Wishlist.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Aggregates.ProductAggregate.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Aggregates.WishlistAggregate.Entities
+{
+    public class Wishlist : BaseEntity, IAggregateRoot
+    {
+        public Guid UserAccountId { get; set; }
+        public List<Product> Products { get; set; } = new List<Product>();
+        public Wishlist() { }
+
+        public Wishlist(Guid userAccountId, List<Product> products)
+        { 
+            this.UserAccountId = userAccountId;
+            this.Products = products;
+
+        }
+    }
+}
