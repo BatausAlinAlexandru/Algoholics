a7f8252495973cbcbf435f0cf75afa0619081198
Minimatrix26 <armand.brudea@yahoo.com>
2025-01-11 14:53:20 +0200

    creat IWishlistRepository idk

--- Domain/Aggregates/WishlistAggregate/Repositories/IWishlistRepository.cs
+++ Domain/Aggregates/WishlistAggregate/Repositories/IWishlistRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Aggregates.ProductAggregate.Entities;
+using Domain.Aggregates.WishlistAggregate.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,10 @@ namespace Domain.Aggregates.WishlistAggregate.Repositories
 {
     public interface IWishlistRepository
     {
-
+        Task<Wishlist> GetWishlistByUserIdAsync(Guid userId);
+        Task<List<Wishlist>> GetAllWishlistsAsync();
+        Task<bool> CreateWishlist(Wishlist wishlist);
+        Task<bool> RemoveWishlist(Guid wishlistId);
+        Task<bool> UpdateWishlist(Guid wishlistId, List<Product> updatedProductList);
     }
 }
